8a3bf5c2a3eec5ffa1d273b67e85abe3ad4a919b
Alexandria Wong <alexandriawhy@gmail.com>
2021-02-18 18:40:42 -0500

    Add uniqueBy function

--- src/exercises/exercise_1_uniqueBy/uniqueBy.js
+++ src/exercises/exercise_1_uniqueBy/uniqueBy.js
@@ -1,3 +1,15 @@
 export default function uniqueBy(data, key) {
   // * Write function here
+  let existedValue = {};
+  let uniqueData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    existedValue[data[i][key]] = i;    
+  }
+
+  for (key in existedValue) {
+    uniqueData.push(data[existedValue[key]]);
+  }
+
+  return uniqueData;
 }
